585d597d55258b7ea56967e4d1b0563605d66bc5
Felipe Bona <felipechiodinibona@hotmail.com>
2023-04-02 20:38:50 -0300

    ajustes sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,23 +1,100 @@
 <template>
-  <b-sidebar id="sidebar" shadow>
-    <div class="container">
-      <b-button :variant="variant('/')" class="text-left w-100 pointer p-2" to="/">Home</b-button>
-      <b-button :variant="variant('/produtos')" class="text-left w-100 pointer p-2" to="/produtos">Produtos</b-button>
-      <b-button :variant="variant('/garcons')" class="text-left w-100 pointer p-2" to="/garcons">Garçons</b-button>
-      <b-button :variant="variant('/combos')" class="text-left w-100 pointer p-2" to="/combos">Combos</b-button>
-      <b-button :variant="variant('/comandas')" class="text-left w-100 pointer p-2" to="/comandas">Comandas</b-button>
-      <b-button :variant="variant('/configuracoes')" class="text-left w-100 pointer p-2" to="/configuracoes">Configurações</b-button>
+  <div class="sidebar" :class="{'collapsed': collapsed}">
+    <div class="sidebar-header">
+      <b-button @click="toggleSidebar">
+        teste
+        <!-- <i :class="{'fa-bars': !collapsed, 'fa-arrow-left': collapsed}"></i> -->
+      </b-button>
     </div>
-  </b-sidebar>
+    <div class="sidebar-body">
+      <b-button variant="transparent" to="/produtos">
+        <b-icon icon="gear-fill"></b-icon>
+        <span>Produtos</span>
+      </b-button>
+      <b-button variant="transparent" to="/produtos">
+        <b-icon icon="gear-fill"></b-icon>
+        <span>Produtos</span>
+      </b-button>
+      <b-button variant="transparent" to="/produtos">
+        <b-icon icon="gear-fill"></b-icon>
+        <span>Produtos</span>
+      </b-button>
+      <b-button variant="transparent" to="/produtos">
+        <b-icon icon="gear-fill"></b-icon>
+        <span>Produtos</span>
+      </b-button>
+      <b-button variant="transparent" to="/produtos">
+        <b-icon icon="gear-fill"></b-icon>
+        <span>Produtos</span>
+      </b-button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      collapsed: false
+    };
+  },
   methods: {
-    variant(route) {
-      if (route == this.$route.path) return 'info'
-      return 'transparent' 
-    } 
+    toggleSidebar() {
+      this.collapsed = !this.collapsed;
+    }
   }
-}
+};
 </script>
+
+<style>
+
+.sidebar {
+  width: 200px;
+  height: 100%;
+  position: fixed;
+  background-color: #ccc;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  transition: width 0.3s ease-in-out;
+}
+
+.sidebar-header {
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid #ddd;
+}
+
+.sidebar-header button {
+  border: none;
+  background-color: transparent;
+  outline: none;
+  cursor: pointer;
+}
+
+.sidebar-header i {
+  font-size: 24px;
+  color: #333;
+  transition: transform 0.3s ease-in-out;
+}
+
+.sidebar-header i.fa-arrow-left {
+  transform: rotate(180deg);
+}
+
+.sidebar-body {
+  /* padding: 20px; */
+  transition: width 0.3s ease-in-out;
+}
+
+.sidebar.collapsed {
+  width: 70px;
+}
+
+.sidebar.collapsed .sidebar-body span {
+  display: none;
+}
+
+</style>
